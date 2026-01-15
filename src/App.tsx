3ed58d2e81c3be5c4acefbdb28e7aeae388f6504
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2026-01-15 19:13:35 +0900

    feat: 라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchPage from "./features/books/pages/SearchPage";
+import SignupPage from "./features/auth/pages/SignupPage";
+import LoginPage from "./features/auth/pages/LoginPage";
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center">
-      <main className="w-full max-w-[70vw]">
-        <SearchPage />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen w-full bg-gray-6">
+        <div className="min-h-screen flex flex-col">
+          <main className="flex-1 flex justify-center items-center">
+            <Routes>
+              <Route path="/" element={<SignupPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/search" element={<SearchPage />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
