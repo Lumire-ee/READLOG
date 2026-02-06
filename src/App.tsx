3270cf817938bb513e28918b989e3c2b0157f868
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2026-02-06 16:30:50 +0900

    feat: Sonner 토스터 전역 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./features/auth/provider/AuthProvider";
 import AppRouter from "./router";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <Toaster position="top-center" />
         <div className="bg-bg-base min-h-screen w-full">
           <div className="flex min-h-screen flex-col">
             <main className="flex flex-1 items-center justify-center">
