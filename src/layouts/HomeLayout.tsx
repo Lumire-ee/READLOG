4c1b8123e34e2d3a18ba43c3258e7b6d5c2aa9eb
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2026-02-06 16:34:12 +0900

    chore: props 이름 오타 수정

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,11 +1,11 @@
 import type { ReactNode } from "react";
 
-type HomeLyaoutProps = {
+type HomeLayoutProps = {
   header: ReactNode;
   children: ReactNode;
 };
 
-export default function HomeLayout({ header, children }: HomeLyaoutProps) {
+export default function HomeLayout({ header, children }: HomeLayoutProps) {
   return (
     <div className="bg-gray-6 min-h-screen w-full">
       <div className="mx-auto w-full max-w-4xl px-4 py-6">
